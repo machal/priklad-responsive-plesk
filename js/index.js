248ed1dc3fceb5715d57dafd303f3a93d80aa535
Martin Michálek <michalek@shortcat.cz>
2013-02-20 20:14:08 +0100

    Podminene natahovani Fancyboxu az na vetsich displejich (768px+) pomoci Modernizru

--- js/index.js
+++ js/index.js
@@ -1,5 +1,42 @@
-$(document).ready(function(){
+(function(){
+  
+  var 
+    ie_version = getInternetExplorerVersion(),
+    big_screen_or_legacy_ie = Modernizr.mq('screen and (min-width: 768px)') || (ie_version == 8);  
+
+  // Nacitani fancyboxu jen na velkych displejich a na MSIE8-
+
+  Modernizr.load({
+
+    test: big_screen_or_legacy_ie,
+
+    yep : [ 
+  		'fancybox/jquery.fancybox.pack.js',
+  		'fancybox/jquery.fancybox.css'
+  	],
 	
-	$(".fancybox").fancybox();
-	
-})
+    complete : function () {
+  		 if (big_screen_or_legacy_ie) {  		   
+  		    // Fancybox
+          $(".fancybox").fancybox();        
+        } 
+     }
+     
+  });
+
+
+  // Vraci verzi IE
+  // http://blogs.msdn.com/b/giorgio/archive/2009/04/14/how-to-detect-ie8-using-javascript-client-side.aspx
+  
+  function getInternetExplorerVersion() {
+      var rv = -1; // Return value assumes failure.
+      if (navigator.appName == 'Microsoft Internet Explorer') {
+          var ua = navigator.userAgent;
+          var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+          if (re.exec(ua) != null)
+              rv = parseFloat(RegExp.$1);
+      }
+      return rv;
+  }
+
+})();
